bbe58132b529779afd8c9959bbf4afb302dbe710
Stefan Härter <sha@rother-oss.com>
2026-03-03 15:26:12 +0100

    Tidied.
    Removed unused variable.

--- Kernel/System/DynamicField/Driver/Lens.pm
+++ Kernel/System/DynamicField/Driver/Lens.pm
@@ -631,11 +631,6 @@ sub GetFieldState {
     my $AttributeFieldValue;
     if ($NeedsReset) {
 
-        my $IsACLReducible = $Self->HasBehavior(
-            DynamicFieldConfig => $DynamicFieldConfig,
-            Behavior           => 'IsACLReducible',
-        );
-
         my $ReferenceID = $DFParam->{ $DynamicFieldConfig->{Config}{ReferenceDFName} } ? $DFParam->{ $DynamicFieldConfig->{Config}{ReferenceDFName} }[0] : undef;
 
         # get the current value of the referenced attribute field if an object is referenced
